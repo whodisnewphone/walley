24e66fb0a131b27b48abfc5e16fcab76d802526f
whodisnewphone <marius.matei@matchi.se>
2025-12-02 02:10:07 +0100

    Added loader.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,35 +1,61 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { TransactionContext } from "../ctx/TransactionContext"
 import TransactionCard from "../components/TransactionCard"
 import FilterSection from "../components/FilterSection"
 
 const Dashboard = () => {
-  const {  displayedTransactions } = useContext(TransactionContext)
+  const { displayedTransactions } = useContext(TransactionContext)
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  useEffect(() => {
+    // Simulate fetching data to show a loader
+    const fetchData = () => {
+      return new Promise<string>((resolve) => {
+        setTimeout(() => {
+          resolve("Data fetched successfully!");
+        }, 1000);
+      });
+    };
+
+    setIsLoading(true);
+
+    fetchData().then(() => {
+      setIsLoading(false);
+    });
+  }, [displayedTransactions]);
+  
   return (
     <>
     <div className="text-center">My Payments Dashboard</div>
     <FilterSection/>
 
     {
-      displayedTransactions?.length === 0 &&
+      !isLoading && displayedTransactions?.length === 0 &&
       <p className="text-center">No transactions found</p>
     }
     {
-      displayedTransactions?.length >0 &&
+      !isLoading && displayedTransactions?.length >0 &&
       <p className="text-center">{displayedTransactions.length} {displayedTransactions.length === 1 ? "transaction" : "transactions"} found</p>
     }
     <div className="container">
-      <div className="transactionList">
-        {
-          displayedTransactions.map(transaction => {
-            return(
-              <TransactionCard transaction={transaction} key={transaction.id}/>
-            )
-          })
-          //todo add pagination
-        }
-      </div>
+      { isLoading &&
+        <div className="text-center">
+          🌀 Loading transactions...
+        </div>
+      }
+      {
+        !isLoading &&
+        <div className="transactionList">
+          {
+            displayedTransactions.map(transaction => {
+              return(
+                <TransactionCard transaction={transaction} key={transaction.id}/>
+              )
+            })
+            //todo add pagination
+          }
+        </div>
+      }
     </div>
     </>
   )
